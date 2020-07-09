c230401c2a233c86b411a322a9dd6e707965f1f4
philanderson888 <philanderson888@hotmail.com>
2020-07-09 06:40:54 +0100

    IntelliJ ReInstall And Hello World

---
+++ HelloWorld/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    // New class com.example.helloworld.HelloWorld and create it
+    // Shift and enter to move down a line
+    public static void main(String[] args) {
+         // control J to see available code templates
+        // control dot to complete a word and add a dot (can do tab then .)
+        // for the last item, press enter to select it
+        // Alt 4 to see the command line window
+        // Shift Shift = search everywhere
+        // Control Control  = run anything ' hello '
+        System.out.println("hello world 2");
+
+        // build a JAR Java Archive  - File, Project Structure or  Shift control alt S
+        // File, Structure, Artifacts, Add, Choose class and OK
+        // Build => Artifacts and check folder out, artifacts, helloworld.jar
+
+    }
+
+}
---
+++ IntelliJWorkspace/src/lab_01_hello_world.java
@@ -0,0 +1,6 @@
+package system;
+public class lab_01_hello_world {
+    public static void main(String[] args) {
+        system.out.println("hello world");
+    }
+}
