0c8e2cc19e4b2f8665bca01db72fbaa0ca82a889
philanderson888 <philanderson888@hotmail.com>
2020-10-03 06:51:50 +0100

    Stock Prices API Project

--- Crude Learning Projects/20200929-api-client-stock-prices/stock-client/src/main/java/uk/co/philanderson/stockclient/WebClientStockClient.java
+++ Crude Learning Projects/20200929-api-client-stock-prices/stock-client/src/main/java/uk/co/philanderson/stockclient/WebClientStockClient.java
@@ -3,6 +3,9 @@ package uk.co.philanderson.stockclient;
 
 import lombok.extern.java.Log;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
@@ -10,9 +13,12 @@ import java.io.IOException;
 import java.time.Duration;
 
 @Log4j2
+@Component
 public class WebClientStockClient {
+
     private WebClient webClient;
 
+    @Autowired
     public WebClientStockClient(WebClient webClient) {
         this.webClient = webClient;
     }
--- Crude Learning Projects/stock-ui/src/main/java/uk/co/philanderson/stockui/ChartController.java
+++ Crude Learning Projects/stock-ui/src/main/java/uk/co/philanderson/stockui/ChartController.java
@@ -1,11 +1,41 @@
 package uk.co.philanderson.stockui;
 
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import org.springframework.stereotype.Component;
+import uk.co.philanderson.stockclient.StockPrice;
+import uk.co.philanderson.stockclient.WebClientStockClient;
+
+import java.util.function.Consumer;
+
 
 @Component
 public class ChartController {
     @FXML
     public LineChart<String,Double> chart;
+    private uk.co.philanderson.stockclient.WebClientStockClient webClientStockClient;
+
+/*    public ChartController(WebClientStockClient webClientStockClient) {
+        this.webClientStockClient = webClientStockClient;
+    }*/
+
+    @FXML
+    public void initialize(){
+        ObservableList<XYChart.Series<String,Double>> data = FXCollections.observableArrayList();
+     //   data.add(new XYChart.Series<>(seriesData));
+        chart.setData(data);
+
+      //  webClientStockClient.pricesFor("SYMBOL").subscribe(this);
+    }
+
+/*    @Override
+    public void accept(StockPrice stockPrice) {
+        Platform.runLater(()->
+            seriesData.add(new XYChart.Data<>(String.valueOf(stockPrice.getTime().getSecond()),stockPrice.getPrice()))
+        );
+    }*/
 }
